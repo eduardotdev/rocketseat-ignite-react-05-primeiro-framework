41f237f9a2cf0c52b339ee5b8a40d18453b20e7a
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-02-04 20:42:46 -0300

    imagens no next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
 
-export default nextConfig;
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+}
+
+export default nextConfig
